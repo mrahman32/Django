f9f0185e5ee80fee47bfcc00ac1286158049ba18
Md. Mostafizur Rahman <mrahman.cse32@gmail.com>
2022-10-13 07:38:45 +0600

    added custom styles in project template folder

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,9 +3,10 @@ from xml.etree.ElementInclude import include
 from django.urls import path
 from . import views
 
+app_name = "polls"
 urlpatterns = [
-    path("", view=views.index, name="index"),
-    path("<int:question_id>/", view=views.detail, name="detail"),
-    path("<int:question_id>/results", view=views.results, name="results"),
-    path("<int:question_id>/vote", view=views.vote, name="vote"),
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", view=views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results", view=views.ResultsView.as_view(), name="results"),
+    path("<int:question_id>/vote/", view=views.vote, name="vote"),
 ]
